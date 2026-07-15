a47192c3bcf5c6a1983b40a748d8a56061c9e77e
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2026-07-15 12:09:18 -0300

    style

--- lib/handoff/pipeline/aggregator.ex
+++ lib/handoff/pipeline/aggregator.ex
@@ -89,7 +89,13 @@ defmodule Handoff.Pipeline.Aggregator do
 
       {_partial, join} ->
         state = cancel_join_timer(state, cid)
-        state = %{state | join: Map.put(join, cid, :timed_out), completed: Map.put(state.completed, cid, {:error, :join_timeout})}
+
+        state = %{
+          state
+          | join: Map.put(join, cid, :timed_out),
+            completed: Map.put(state.completed, cid, {:error, :join_timeout})
+        }
+
         state = schedule_join_gc(state, cid)
         {emitted, state} = drain_ready(state, [])
         {:noreply, emitted, state}
@@ -155,7 +161,7 @@ defmodule Handoff.Pipeline.Aggregator do
       output = format_output(state, partial)
 
       state = clear_join(state, cid)
-      state = %{state | completed: Map.put(state.completed, cid, output)}
+      %{state | completed: Map.put(state.completed, cid, output)}
     else
       %{state | join: Map.put(state.join, cid, partial)}
     end
